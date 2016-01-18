a5378a9fc922400a3543d8289295e042e8e3915f
goldjee <ziglee@mail.ru>
2016-01-18 09:28:26 +0300

    Added some notation and new methods

--- src/main/java/locomotive/dota2/matchAPI.java
+++ src/main/java/locomotive/dota2/matchAPI.java
@@ -166,7 +166,27 @@ public class matchAPI {
     public static JSONObject getMatchHistory(String apiKey, JSONObject params) {
 //        hero_id               uint32    The ID of the hero that must be in the matches being queried
 //        game_mode             uint32    Which game mode to return matches for
+//            0 - None
+//            1 - All Pick
+//            2 - Captain's Mode
+//            3 - Random Draft
+//            4 - Single Draft
+//            5 - All Random
+//            6 - Intro
+//            7 - Diretide
+//            8 - Reverse Captain's Mode
+//            9 - The Greeviling
+//            10 - Tutorial
+//            11 - Mid Only
+//            12 - Least Played
+//            13 - New Player Pool
+//            14 - Compendium Matchmaking
+//            16 - Captain's Draft
 //        skill	                uint32    The average skill range of the match, these can be [1-3] with lower numbers being lower skill. Ignored if an account ID is specified
+//            0 - Any
+//            1 - Normal
+//            2 - High
+//            3 - Very High
 //        min_players           string    Minimum number of human players that must be in a match for it to be returned
 //        account_id            string    An account ID to get matches from. This will fail if the user has their match history hidden
 //        league_id             string    The league ID to return games from
@@ -175,6 +195,55 @@ public class matchAPI {
 //        tournament_games_only string    Whether or not tournament games should only be returned
 
         String action = "get latest matches";
+
+        String methodGroup = "IDOTA2Match_570";
+        String method = "GetMatchHistory";
+        String version = "v1";
+
+        String url = apiBase +
+                methodGroup + "/" +
+                method + "/" +
+                version + "/?" +
+                "key=" + apiKey +
+                "&format=" + format;
+
+        Iterator<String> keys = params.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            url += ("&" + key + "=" + params.get(key).toString());
+        }
+
+        JSONObject request = new JSONObject();
+        request.put("action", action);
+        request.put("url", url);
+
+        return request;
+    }
+
+    public static JSONObject getMatchHistoryBySequenceNum(String apiKey) {
+        String action = "get latest matches by their record sequence";
+
+        String methodGroup = "IDOTA2Match_570";
+        String method = "GetMatchHistoryBySequenceNum";
+        String version = "v1";
+
+        String url = apiBase +
+                methodGroup + "/" +
+                method + "/" +
+                version + "/?" +
+                "key=" + apiKey +
+                "&format=" + format;
+
+        JSONObject request = new JSONObject();
+        request.put("action", action);
+        request.put("url", url);
+
+        return request;
+    }
+    public static JSONObject getMatchHistoryBySequenceNum(String apiKey, JSONObject params) {
+//        start_at_match_seq_num uint64    The minimum match ID to start from
+//        matches_requested      string    The number of requested matches to return
+        String action = "get latest matches by their record sequence";
 
         String methodGroup = "IDOTA2Match_570";
         String method = "GetMatchHistory";
